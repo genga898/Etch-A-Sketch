cf3c25e443513c0777598f9eecf1ae6cd3c0391f
genga898 <genga898@gmail.com>
2022-08-01 13:09:53 +0300

    Add eventListener to check for hover

--- script.js
+++ script.js
@@ -6,5 +6,13 @@ for (let i = 0; i < gridSlots; i++) {
     const slot = document.createElement("div");
     slot.classList.add("slot");
     container.appendChild(slot);
+    slot.style.backgroundColor = "white";
+    slot.style.margin = "0.5px";
+    slot.style.borderRadius = "2px";
+    document.addEventListener("mouseover", function() {
+        slot.style.backgroundColor = "blue";
+    })
+    document.addEventListener("mouseout", function() {
+        slot.style.backgroundColor = "white"; 
+        })
 }
-
