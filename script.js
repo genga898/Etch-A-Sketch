56061ffdf9f8e1e271a38cdaa492a94b64f471d4
genga898 <genga898@gmail.com>
2022-08-02 12:21:22 +0300

    Create createDiv function and update the eventListener

--- script.js
+++ script.js
@@ -1,18 +1,35 @@
 const container = document.querySelector(".container");
-const gridSlots = (16*16);
+const grid = 16*16;
+const inputBtn = document.querySelector("#button");
+inputBtn.addEventListener("click", () => {
+    let answer = prompt("Enter the grid size you want to display: ");
+    if (!(answer > 0 && answer < 100)) {
+        alert("Please enter a number between 1 and 100.");
+    }else if (answer > 0 && answer < 100) {
+        let gridSize = answer**2;
+        const slots =Math.sqrt(gridSize);
+        const columnSize = 720/slots;
+        const rowSize = 720/slots;
+        createDiv(gridSize);
+        container.style.setProperty("grid-template-columns", `repeat(${slots}, ${columnSize}px)`);
+        container.style.setProperty("grid-template-rows", `repeat(${slots}, ${rowSize}px)`);
+    }
+
+    });
+
 
 //Creating multiple divs
-for (let i = 0; i < gridSlots; i++) {
-    const slot = document.createElement("div");
-    slot.classList.add("slot");
-    container.appendChild(slot);
-    slot.style.backgroundColor = "white";
-    slot.style.margin = "0.5px";
-    slot.style.borderRadius = "2px";
-    document.addEventListener("mouseover", function() {
-        slot.style.backgroundColor = "blue";
-    })
-    document.addEventListener("mouseout", function() {
-        slot.style.backgroundColor = "white"; 
-        })
+function createDiv(gridSize){
+    for(let i = 0; i < gridSize; i++) {
+        const slot = document.createElement("div");
+        slot.classList.add("slot");
+        container.appendChild(slot);
+        slot.style.backgroundColor = "white";
+        slot.style.margin = "0.5px";
+        slot.style.borderRadius = "2px";
+        slot.addEventListener("mouseover", function(){
+            slot.style.backgroundColor = "grey";
+        });
+    }
 }
+createDiv(grid);
